0db2eee132783092f4b4f364904df58f08c0bd6f
Chaidaris <2131710102@student.polinema.ac.id>
2024-03-07 13:52:55 +0700

    update jobsheet4 praktikum 2.3.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,31 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 2,
-        // ];
-        // UserModel::create($data);
-
-        // // $data = [
-        // //     'nama' => 'Pelanggan Pertama',
-        // // ];
-        // // UserModel::where('username', 'customer-1')->update($data);
-
-        // $user = UserModel::all();
-        // $user = UserModel::find(1);
-        // $user = UserModel::where('level_id', 1)->first();
-        // $user = UserModel::firstwhere('level_id', 1);
-
-        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
-        //     abort(404);
-        // });
-
-        // $user = UserModel::findOrFail(1);
-
-        $user = UserModel::where('username', 'manager10')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
 
 
