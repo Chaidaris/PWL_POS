9516c35a7d0f499c00a5b9f39db7cd690279d171
Chaidaris <2131710102@student.polinema.ac.id>
2024-03-28 15:24:36 +0700

    update jobsheet7 praktikum4

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -18,6 +18,22 @@
             {{ session('error') }}
         </div>
         @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter</label>
+                    <div class="col-3">
+                        <select type="text" class="form-control" id="level_id" name="level_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach($level as $item)
+                            <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level pengguna</small>
+                    </div>
+                </div>
+            </div>
+        </div>
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
@@ -38,44 +54,45 @@
 <script>
     $(document).ready(function() {
         var dataUser = $('#table_user').DataTable({
-            serverSide: true, // serverSide: true, jika ingin menggunakan server side processing
+            serverSide: true,
             ajax: {
                 "url": "{{ url('user/list') }}",
                 "dataType": "json",
-                "type": "POST"
-            },
-            columns: [
-                {
-                    data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()
-                    className: "text-center",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "username",
-                    className: "",
-                    orderable: true, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: true // searchable: true, jika ingin kolom ini bisa dicari
-                },
-                {
-                    data: "nama",
-                    className: "",
-                    orderable: true, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: true // searchable: true, jika ingin kolom ini bisa dicari
-                },
-                {
-                    data: "level.level_nama",
-                    className: "",
-                    orderable: false, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: false // searchable: true, jika ingin kolom ini bisa dicari
-                },
-                {
-                    data: "aksi",
-                    className: "",
-                    orderable: false, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: false // searchable: true, jika ingin kolom ini bisa dicari
+                "type": "POST",
+                "data": function(d) {
+                    d.level_id = $('#level_id').val();
                 }
-            ]
+            },
+            columns: [{
+                data: "DT_RowIndex",
+                className: "text-center",
+                orderable: false,
+                searchable: false
+            }, {
+                data: "username",
+                className: "",
+                orderable: true,
+                searchable: true
+            }, {
+                data: "nama",
+                className: "",
+                orderable: true,
+                searchable: true
+            }, {
+                data: "level.level_nama",
+                className: "",
+                orderable: false,
+                searchable: false
+            }, {
+                data: "aksi",
+                className: "",
+                orderable: false,
+                searchable: false
+            }]
+        });
+
+        $('#level_id').on('change', function() {
+            dataUser.ajax.reload();
         });
     });
 </script>
